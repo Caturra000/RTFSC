6d8aa3316c7a3e537a15f670d829cf5595ee6e40
Caturra <wasdjkl233@gmail.com>
2021-02-17 02:20:41 +0800

    添加sort注解-insertion部分

--- STL/libstdc++/algorithm/sort流程.cpp
+++ STL/libstdc++/algorithm/sort流程.cpp
@@ -1,3 +1,15 @@
+// TL;DR 
+// sort的具体实现为Introspective Sorting，并非纯粹的快排
+// 源码中不仅能了解内省式排序的具体实现，还有一些技巧可借鉴，
+// 包括但不限于玄学调参、if分支消去、递归和迭代混着使用的做法
+// <del>另外可学到令人血压飙升的代码风格</del>
+
+// 代码展开是按照自顶向下的调用顺序
+// 分析顺序不推荐按照这个流程走，已用STEP标记顺序
+
+
+// STEP0和STEP1分析带有先判断first优化的插入排序流程
+ 
   /**
    *  @brief Sort the elements of a sequence.
    *  @ingroup sorting_algorithms
@@ -24,10 +36,12 @@
       __glibcxx_requires_valid_range(__first, __last);
       __glibcxx_requires_irreflexive(__first, __last);
 
-      std::__sort(__first, __last, __gnu_cxx::__ops::__iter_less_iter());
+      std::__sort(__first, __last, __gnu_cxx::__ops::__iter_less_iter()); // comp()使用*it1 < *it2
     }
 
 
+  // sort真正的实现，可以称之为Introspective Sorting
+  // 小于等于_S_threshold直接进行最朴素的插入排序（跳过__introsort_loop，只进入__final_insertion_sort不含优化的分支）
   template<typename _RandomAccessIterator, typename _Compare>
     inline void
     __sort(_RandomAccessIterator __first, _RandomAccessIterator __last,
@@ -36,12 +50,14 @@
       if (__first != __last)
 	{
 	  std::__introsort_loop(__first, __last,
-				std::__lg(__last - __first) * 2,   // 玄学参数
+        // https://stackoverflow.com/questions/40434664/what-is-std-lg
+				std::__lg(__last - __first) * 2,   // 玄学参数，恕我没看懂这个取log实现，从结论上可认为是2*log_2(last-first)吧
 				__comp);
 	  std::__final_insertion_sort(__first, __last, __comp);
 	}
     }
 
+//////////////////////// 主要的两个步骤
 
   /// This is a helper function for the sort routine.
   // 只有大于_S_threshold的个数才会执行
@@ -64,7 +80,7 @@
 	  _RandomAccessIterator __cut =
 	    std::__unguarded_partition_pivot(__first, __last, __comp);
 	  std::__introsort_loop(__cut, __last, __depth_limit, __comp);
-	  __last = __cut;
+	  __last = __cut; // 用while来替代尾递归__introsort_loop(__first, __cut, __depth_limit, __comp)，但我寻思编译器会优化了吧
 	}
     }
 
@@ -78,6 +94,9 @@
       if (__last - __first > int(_S_threshold))
 	{
 	  std::__insertion_sort(__first, __first + int(_S_threshold), __comp);
+    // 后面绝大多数元素都减少边界检查以提高性能
+    // Question. 这里直接unguarded是安全的吗，是因为快排后才安全？
+    // 必须要求[first, first+_S_threshold)中肯定存在有比[first+_S_threshold, last)都更小的值
 	  std::__unguarded_insertion_sort(__first + int(_S_threshold), __last,
 					  __comp);
 	}
@@ -86,9 +105,9 @@
     }
 
 
+//////////////////////// 具体实现展开
 
-
-// TODO __introsort_loop部分展开
+// TODO __introsort_loop部分展开（递归深度阈值内）
 
 
 
@@ -136,7 +155,10 @@
 
 // TODO __final_insertion_sort部分展开
 
+
+// STEP 0
   /// This is a helper function for the sort routine.
+  // 先判断first再进行常规插入排序
   template<typename _RandomAccessIterator, typename _Compare>
     void
     __insertion_sort(_RandomAccessIterator __first,
@@ -144,22 +166,29 @@
     {
       if (__first == __last) return;
 
+      // [first, i)已处理
       for (_RandomAccessIterator __i = __first + 1; __i != __last; ++__i)
 	{
-	  if (__comp(__i, __first))
+	  if (__comp(__i, __first)) // first后某个i满足 *i < *first逆序     // ##flag #0
 	    {
 	      typename iterator_traits<_RandomAccessIterator>::value_type
 		__val = _GLIBCXX_MOVE(*__i);
-	      _GLIBCXX_MOVE_BACKWARD3(__first, __i, __i + 1);
-	      *__first = _GLIBCXX_MOVE(__val);
+	      _GLIBCXX_MOVE_BACKWARD3(__first, __i, __i + 1); // moved [first, i) into i+1 区间右移
+	      *__first = _GLIBCXX_MOVE(__val); // 上面空出一个位置，val插入到first
+        // 一次性move过去，避免额外的逐个移动还要检查的次数（效率存疑）
 	    }
-	  else
+	  else 
+      // *i比*first大，但不保证(first, i)中与i没有逆序，因此需要常规地线性往前遍历，最多到first（肯定不符合##flag #1的条件）
+      // 这样可以减少多次while时额外检查next==first的次数
 	    std::__unguarded_linear_insert(__i,
 				__gnu_cxx::__ops::__val_comp_iter(__comp));
 	}
     }
 
+
+// STEP 1
   /// This is a helper function for the sort routine.
+  // 常规的插入排序流程
   template<typename _RandomAccessIterator, typename _Compare>
     void
     __unguarded_linear_insert(_RandomAccessIterator __last,
@@ -168,18 +197,21 @@
       typename iterator_traits<_RandomAccessIterator>::value_type
 	__val = _GLIBCXX_MOVE(*__last);
       _RandomAccessIterator __next = __last;
-      --__next;
-      while (__comp(__val, __next))
+      --__next; // next为前一个迭代器
+      while (__comp(__val, __next))  // 仍然逆序 // ##flag #1
 	{
+    // 整个过程就是std::swap(*last--, *next--);
 	  *__last = _GLIBCXX_MOVE(*__next);
-	  __last = __next;
+	  __last = __next; // last往前走一步，缩小规模
 	  --__next;
+    // 在这个过程中，val是固定的值，而且是局部的变量，cache友好一点，不必同时取两个迭代器的值
 	}
-      *__last = _GLIBCXX_MOVE(__val);
+      *__last = _GLIBCXX_MOVE(__val); // sorted
     }
 
 
   /// This is a helper function for the sort routine.
+  // 这个过程必须保证first之前（不含first）有一个更小的值存在
   template<typename _RandomAccessIterator, typename _Compare>
     inline void
     __unguarded_insertion_sort(_RandomAccessIterator __first,
@@ -189,6 +221,154 @@
 	std::__unguarded_linear_insert(__i,
 				__gnu_cxx::__ops::__val_comp_iter(__comp));
     }
+
+
+
+
+
+// TODO 递归深度阈值外
+
+  template<typename _RandomAccessIterator, typename _Compare>
+    inline void
+    __partial_sort(_RandomAccessIterator __first,
+		   _RandomAccessIterator __middle,
+		   _RandomAccessIterator __last,
+		   _Compare __comp)
+    {
+      std::__heap_select(__first, __middle, __last, __comp);
+      std::__sort_heap(__first, __middle, __comp);
+    }
+
+  /// This is a helper function for the sort routines.
+  template<typename _RandomAccessIterator, typename _Compare>
+    void
+    __heap_select(_RandomAccessIterator __first,
+		  _RandomAccessIterator __middle,
+		  _RandomAccessIterator __last, _Compare __comp)
+    {
+      std::__make_heap(__first, __middle, __comp);
+      for (_RandomAccessIterator __i = __middle; __i < __last; ++__i)
+	if (__comp(__i, __first))
+	  std::__pop_heap(__first, __middle, __i, __comp);
+    }
+
+  template<typename _RandomAccessIterator, typename _Compare>
+    void
+    __sort_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
+		_Compare& __comp)
+    {
+      while (__last - __first > 1)
+	{
+	  --__last;
+	  std::__pop_heap(__first, __last, __last, __comp);
+	}
+    }
+
+
+
+
+
+// BONUS: 堆排更多细节
+
+  template<typename _RandomAccessIterator, typename _Compare>
+    void
+    __make_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
+		_Compare& __comp)
+    {
+      typedef typename iterator_traits<_RandomAccessIterator>::value_type
+	  _ValueType;
+      typedef typename iterator_traits<_RandomAccessIterator>::difference_type
+	  _DistanceType;
+
+      if (__last - __first < 2)
+	return;
+
+      const _DistanceType __len = __last - __first;
+      _DistanceType __parent = (__len - 2) / 2;
+      while (true)
+	{
+	  _ValueType __value = _GLIBCXX_MOVE(*(__first + __parent));
+	  std::__adjust_heap(__first, __parent, __len, _GLIBCXX_MOVE(__value),
+			     __comp);
+	  if (__parent == 0)
+	    return;
+	  __parent--;
+	}
+    }
+
+
+
+  template<typename _RandomAccessIterator, typename _Distance,
+	   typename _Tp, typename _Compare>
+    void
+    __adjust_heap(_RandomAccessIterator __first, _Distance __holeIndex,
+		  _Distance __len, _Tp __value, _Compare __comp)
+    {
+      const _Distance __topIndex = __holeIndex;
+      _Distance __secondChild = __holeIndex;
+      while (__secondChild < (__len - 1) / 2)
+	{
+	  __secondChild = 2 * (__secondChild + 1);
+	  if (__comp(__first + __secondChild,
+		     __first + (__secondChild - 1)))
+	    __secondChild--;
+	  *(__first + __holeIndex) = _GLIBCXX_MOVE(*(__first + __secondChild));
+	  __holeIndex = __secondChild;
+	}
+      if ((__len & 1) == 0 && __secondChild == (__len - 2) / 2)
+	{
+	  __secondChild = 2 * (__secondChild + 1);
+	  *(__first + __holeIndex) = _GLIBCXX_MOVE(*(__first
+						     + (__secondChild - 1)));
+	  __holeIndex = __secondChild - 1;
+	}
+      __decltype(__gnu_cxx::__ops::__iter_comp_val(_GLIBCXX_MOVE(__comp)))
+	__cmp(_GLIBCXX_MOVE(__comp));
+      std::__push_heap(__first, __holeIndex, __topIndex,
+		       _GLIBCXX_MOVE(__value), __cmp);
+    }
+
+
+
+  // Heap-manipulation functions: push_heap, pop_heap, make_heap, sort_heap,
+  // + is_heap and is_heap_until in C++0x.
+
+  template<typename _RandomAccessIterator, typename _Distance, typename _Tp,
+	   typename _Compare>
+    void
+    __push_heap(_RandomAccessIterator __first,
+		_Distance __holeIndex, _Distance __topIndex, _Tp __value,
+		_Compare& __comp)
+    {
+      _Distance __parent = (__holeIndex - 1) / 2;
+      while (__holeIndex > __topIndex && __comp(__first + __parent, __value))
+	{
+	  *(__first + __holeIndex) = _GLIBCXX_MOVE(*(__first + __parent));
+	  __holeIndex = __parent;
+	  __parent = (__holeIndex - 1) / 2;
+	}
+      *(__first + __holeIndex) = _GLIBCXX_MOVE(__value);
+    }
+
+
+
+  template<typename _RandomAccessIterator, typename _Compare>
+    inline void
+    __pop_heap(_RandomAccessIterator __first, _RandomAccessIterator __last,
+	       _RandomAccessIterator __result, _Compare& __comp)
+    {
+      typedef typename iterator_traits<_RandomAccessIterator>::value_type
+	_ValueType;
+      typedef typename iterator_traits<_RandomAccessIterator>::difference_type
+	_DistanceType;
+
+      _ValueType __value = _GLIBCXX_MOVE(*__result);
+      *__result = _GLIBCXX_MOVE(*__first);
+      std::__adjust_heap(__first, _DistanceType(0),
+			 _DistanceType(__last - __first),
+			 _GLIBCXX_MOVE(__value), __comp);
+    }
+
 
 
 // TODO __introselect只在nth_element中调用
