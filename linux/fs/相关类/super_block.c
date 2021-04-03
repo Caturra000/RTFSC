282403d8ee4554f88df4a9d308dbb84da50c4042
Caturra <wasdjkl233@gmail.com>
2021-04-04 01:37:36 +0800

    完善super_block成员注释

--- linux/fs/相关类/super_block.c
+++ linux/fs/相关类/super_block.c
@@ -2,34 +2,34 @@
 // 版本：4.18.20
 
 struct super_block {
-	struct list_head	s_list;		/* Keep this first */
-	dev_t			s_dev;		/* search index; _not_ kdev_t */
-	unsigned char		s_blocksize_bits;
-	unsigned long		s_blocksize;
+	struct list_head	s_list;		/* Keep this first */ // 遍历所有sb对象
+	dev_t			s_dev;		/* search index; _not_ kdev_t */ // 存储sb的块设备
+	unsigned char		s_blocksize_bits; // fs的块长度的位数
+	unsigned long		s_blocksize; // fs的块长度
 	loff_t			s_maxbytes;	/* Max file size */
-	struct file_system_type	*s_type;
+	struct file_system_type	*s_type; // 属于（指回）哪个type
 	const struct super_operations	*s_op;
-	const struct dquot_operations	*dq_op;
-	const struct quotactl_ops	*s_qcop;
-	const struct export_operations *s_export_op;
-	unsigned long		s_flags;
+	const struct dquot_operations	*dq_op; // 指向磁盘配额操作表的指针
+	const struct quotactl_ops	*s_qcop; // 指向磁盘配额管理操作表的指针
+	const struct export_operations *s_export_op; // 导出操作，应用于NFS
+	unsigned long		s_flags; // 装载标志
 	unsigned long		s_iflags;	/* internal SB_I_* flags */
 	unsigned long		s_magic;
-	struct dentry		*s_root;
-	struct rw_semaphore	s_umount;
-	int			s_count;
-	atomic_t		s_active;
+	struct dentry		*s_root; // fs根目录的dentry
+	struct rw_semaphore	s_umount; // 卸载用信号量
+	int			s_count; // 真正的引用计数，决定sb能否释放
+	atomic_t		s_active; // 活动引用技数，表示被mount的次数
 #ifdef CONFIG_SECURITY
 	void                    *s_security;
 #endif
-	const struct xattr_handler **s_xattr;
+	const struct xattr_handler **s_xattr; // 用于扩展
 #if IS_ENABLED(CONFIG_FS_ENCRYPTION)
 	const struct fscrypt_operations	*s_cop;
 #endif
 	struct hlist_bl_head	s_roots;	/* alternate root dentries for NFS */
 	struct list_head	s_mounts;	/* list of mounts; _not_ for fs use */
-	struct block_device	*s_bdev;
-	struct backing_dev_info *s_bdi;
+	struct block_device	*s_bdev; // 对于磁盘的fs，为指向块设备描述符的指针，否则NULL
+	struct backing_dev_info *s_bdi; // 指向后备信息描述符的指针
 	struct mtd_info		*s_mtd;
 	struct hlist_node	s_instances;
 	unsigned int		s_quota_types;	/* Bitmask of supported quota types */
@@ -37,12 +37,12 @@ struct super_block {
 
 	struct sb_writers	s_writers;
 
-	char			s_id[32];	/* Informational name */
+	char			s_id[32];	/* Informational name */ // 如果是磁盘文件系统，则为块设备名，否则为文件系统类型名
 	uuid_t			s_uuid;		/* UUID */
 
-	void 			*s_fs_info;	/* Filesystem private info */
+	void 			*s_fs_info;	/* Filesystem private info */ // 私有实现
 	unsigned int		s_max_links;
-	fmode_t			s_mode;
+	fmode_t			s_mode; // 记录装载模式（只读，写/读）
 
 	/* Granularity of c/m/atime in ns.
 	   Cannot be worse than a second */
