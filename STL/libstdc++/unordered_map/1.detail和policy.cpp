096dbd598cc4658315ca77b88e141d8170f2dd75
Caturra <wasdjkl233@gmail.com>
2021-06-24 19:34:15 +0800

    add libstdc++/hashtable prime

--- STL/libstdc++/unordered_map/1.detail和policy.cpp
+++ STL/libstdc++/unordered_map/1.detail和policy.cpp
@@ -106,7 +106,10 @@
 // 对于刚插入的情况，bucket数目显然不会增加
 // 本来构造好的_M_buckets是nullptr，插入后 _M_buckets[__bkt] = &_M_before_begin（一个预设节点）; __bkt=0 此时_M_buckets才含有_M_before_begin和插入的node(before begin.next)
 // （好累啊，写的什么鬼，直接debug跟踪算了吧）
-// TODO rehash
+
+// rehash默认使用_Prime_rehash_policy
+// 然而这个没法在hashtable和hashtable_policy文件找到 ##flag #2
+// 查了gcc的changelog，发现它的部分实现移到了hashtable_c++0x.cc，已加到本文件中
 
 
 #ifndef _HASHTABLE_POLICY_H
@@ -548,7 +551,7 @@ namespace __detail
     // increase bucket count?  If so, return make_pair(true, n), where n
     // is the new bucket count.  If not, return make_pair(false, 0).
     std::pair<bool, std::size_t>
-    _M_need_rehash(std::size_t __n_bkt, std::size_t __n_elt,
+    _M_need_rehash(std::size_t __n_bkt, std::size_t __n_elt,  // ##flag #2
 		   std::size_t __n_ins) const;
 
     typedef std::size_t _State;
@@ -1061,3 +1064,110 @@ _GLIBCXX_END_NAMESPACE_VERSION
 } // namespace std
 
 #endif // _HASHTABLE_POLICY_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace std _GLIBCXX_VISIBILITY(default)
+{
+_GLIBCXX_BEGIN_NAMESPACE_VERSION
+
+#include "../shared/hashtable-aux.cc"
+
+namespace __detail
+{
+  // Return a prime no smaller than n.
+  std::size_t
+  _Prime_rehash_policy::_M_next_bkt(std::size_t __n) const
+  {
+    // Optimize lookups involving the first elements of __prime_list.
+    // (useful to speed-up, eg, constructors)
+    static const unsigned char __fast_bkt[] // trick. 无需lower_bound
+      = { 2, 2, 2, 3, 5, 5, 7, 7, 11, 11, 11, 11, 13, 13 };
+
+    if (__n < sizeof(__fast_bkt))
+      {
+	if (__n == 0)
+	  // Special case on container 1st initialization with 0 bucket count
+	  // hint. We keep _M_next_resize to 0 to make sure that next time we
+	  // want to add an element allocation will take place.
+	  return 1;
+
+	_M_next_resize =
+	  __builtin_floor(__fast_bkt[__n] * (double)_M_max_load_factor);
+	return __fast_bkt[__n];
+      }
+
+    // Number of primes (without sentinel).
+    constexpr auto __n_primes
+      = sizeof(__prime_list) / sizeof(unsigned long) - 1;
+
+    // Don't include the last prime in the search, so that anything
+    // higher than the second-to-last prime returns a past-the-end
+    // iterator that can be dereferenced to get the last prime.
+    constexpr auto __last_prime = __prime_list + __n_primes - 1;
+
+    const unsigned long* __next_bkt =
+      std::lower_bound(__prime_list + 6, __last_prime, __n);
+
+    if (__next_bkt == __last_prime)
+      // Set next resize to the max value so that we never try to rehash again
+      // as we already reach the biggest possible bucket number.
+      // Note that it might result in max_load_factor not being respected.
+      _M_next_resize = size_t(-1);
+    else
+      _M_next_resize =
+	__builtin_floor(*__next_bkt * (double)_M_max_load_factor);
+
+    return *__next_bkt;
+  }
+
+  // Finds the smallest prime p such that alpha p > __n_elt + __n_ins.
+  // If p > __n_bkt, return make_pair(true, p); otherwise return
+  // make_pair(false, 0).  In principle this isn't very different from
+  // _M_bkt_for_elements.
+
+  // The only tricky part is that we're caching the element count at
+  // which we need to rehash, so we don't have to do a floating-point
+  // multiply for every insertion.
+
+  std::pair<bool, std::size_t>
+  _Prime_rehash_policy::
+  _M_need_rehash(std::size_t __n_bkt, std::size_t __n_elt,
+		 std::size_t __n_ins) const
+  {
+    if (__n_elt + __n_ins > _M_next_resize)
+      {
+	// If _M_next_resize is 0 it means that we have nothing allocated so
+	// far and that we start inserting elements. In this case we start
+	// with an initial bucket size of 11.
+	double __min_bkts
+	  = std::max<std::size_t>(__n_elt + __n_ins, _M_next_resize ? 0 : 11)
+	  / (double)_M_max_load_factor;
+	if (__min_bkts >= __n_bkt)
+	  return { true,
+	    _M_next_bkt(std::max<std::size_t>(__builtin_floor(__min_bkts) + 1,
+					      __n_bkt * _S_growth_factor)) };
+
+	_M_next_resize
+	  = __builtin_floor(__n_bkt * (double)_M_max_load_factor);
+	return { false, 0 };
+      }
+    else
+      return { false, 0 };
+  }
+} // namespace __detail
+
+_GLIBCXX_END_NAMESPACE_VERSION
+} // namespace std
