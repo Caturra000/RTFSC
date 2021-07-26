63cd388b4480f1e189429803f4e7eea2c7d11f35
Caturra <wasdjkl233@gmail.com>
2021-07-26 17:15:13 +0800

    add linux/ext2 aops

---
+++ linux/fs/ext2/a_op.c
@@ -0,0 +1,15 @@
+// 文件：/fs/ext2/inode.c
+
+const struct address_space_operations ext2_aops = {
+	.readpage		= ext2_readpage,
+	.readpages		= ext2_readpages,
+	.writepage		= ext2_writepage,
+	.write_begin		= ext2_write_begin,
+	.write_end		= ext2_write_end,
+	.bmap			= ext2_bmap,
+	.direct_IO		= ext2_direct_IO,
+	.writepages		= ext2_writepages,
+	.migratepage		= buffer_migrate_page,
+	.is_partially_uptodate	= block_is_partially_uptodate,
+	.error_remove_page	= generic_error_remove_page,
+};
