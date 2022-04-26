1932f36a246a7aceac9b6910d37d5bde500fb57e
Caturra <wasdjkl233@gmail.com>
2022-04-26 11:11:41 +0800

    update linux/buddy init

--- linux/mm/buddy/初始化流程/memblock释放.c
+++ linux/mm/buddy/初始化流程/memblock释放.c
@@ -5,11 +5,14 @@
  *
  * Returns the number of pages actually released.
  */
+// buddy的初始化状态由memblock/bootmem提供
+// 该函数被/arch/x86/mm/init_64.c中的mem_init(void)调用
 unsigned long __init free_all_bootmem(void)
 {
 	unsigned long total_pages = 0;
 	bootmem_data_t *bdata;
 
+	// 大概是把所有zone的页面数据清0
 	reset_all_zones_managed_pages();
 
 	list_for_each_entry(bdata, &bdata_list, list)
@@ -28,6 +31,7 @@ static unsigned long __init free_all_bootmem_core(bootmem_data_t *bdata)
 	if (!bdata->node_bootmem_map)
 		return 0;
 
+	// 算是一个用于表示page的bitmap
 	map = bdata->node_bootmem_map;
 	start = bdata->node_min_pfn;
 	end = bdata->node_low_pfn;
@@ -47,6 +51,7 @@ static unsigned long __init free_all_bootmem_core(bootmem_data_t *bdata)
 		 */
 		vec = ~map[idx / BITS_PER_LONG];
 
+		// 是不是写复杂了？
 		if (shift) {
 			vec >>= shift;
 			if (end - start >= BITS_PER_LONG)
@@ -58,7 +63,9 @@ static unsigned long __init free_all_bootmem_core(bootmem_data_t *bdata)
 		 * BITS_PER_LONG block of pages in front of us, free
 		 * it in one go.
 		 */
+		// 存在刚好全1的bitmap
 		if (IS_ALIGNED(start, BITS_PER_LONG) && vec == ~0UL) {
+			// order为log2(64)
 			int order = ilog2(BITS_PER_LONG);
 
 			__free_pages_bootmem(pfn_to_page(start), start, order);
@@ -68,6 +75,7 @@ static unsigned long __init free_all_bootmem_core(bootmem_data_t *bdata)
 			cur = start;
 
 			start = ALIGN(start + 1, BITS_PER_LONG);
+			// 如果bitmap单次迭代中存在空洞（0），则设order为0，逐个加入
 			while (vec && cur != start) {
 				if (vec & 1) {
 					page = pfn_to_page(cur);
@@ -80,9 +88,11 @@ static unsigned long __init free_all_bootmem_core(bootmem_data_t *bdata)
 		}
 	}
 
+	// 剩下的一些边角料
 	cur = bdata->node_min_pfn;
 	page = virt_to_page(bdata->node_bootmem_map);
 	pages = bdata->node_low_pfn - bdata->node_min_pfn;
+	// bootmem_bootmap_pages - calculate bitmap size in pages
 	pages = bootmem_bootmap_pages(pages);
 	count += pages;
 	while (pages--)
@@ -113,6 +123,7 @@ static void __init __free_pages_boot_core(struct page *page, unsigned int order)
 	prefetchw(p);
 	for (loop = 0; loop < (nr_pages - 1); loop++, p++) {
 		prefetchw(p + 1);
+		// TODO page的reserved标记
 		__ClearPageReserved(p);
 		set_page_count(p, 0);
 	}
@@ -121,5 +132,7 @@ static void __init __free_pages_boot_core(struct page *page, unsigned int order)
 
 	page_zone(page)->managed_pages += nr_pages;
 	set_page_refcounted(page);
+	// 其实是复用buddy的释放接口
+	// 这部分以后单独再看吧
 	__free_pages(page, order);
 }
