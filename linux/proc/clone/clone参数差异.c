03831be742d516113657bd936b1ac19b13cb55cc
Caturra <wasdjkl233@gmail.com>
2022-11-02 12:42:41 +0800

    update linux/clone kthread flags

--- linux/proc/clone/clone参数差异.c
+++ linux/proc/clone/clone参数差异.c
@@ -29,6 +29,26 @@ SYSCALL_DEFINE0(vfork)
 #endif
 
 
+
+
+
+
+
 // pthread库
 // 简单的用strace调用看了下pthread_create
 // CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|CLONE_SYSVSEM|CLONE_SETTLS|CLONE_PARENT_SETTID|CLONE_CHILD_CLEARTID
+
+
+
+
+
+
+
+// kthread
+// kthreadd创建时调用pid = kernel_thread(kthread, create, CLONE_FS | CLONE_FILES | SIGCHLD);
+// 既CLONE_VM | CLONE_FS | CLONE_FILES | SIGCHLD | CLONE_UNTRACED
+pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)
+{
+	return _do_fork(flags|CLONE_VM|CLONE_UNTRACED, (unsigned long)fn,
+		(unsigned long)arg, NULL, NULL, 0);
+}
