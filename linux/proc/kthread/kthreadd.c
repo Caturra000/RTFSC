73a2ee1803e3b6dbaa14b93f58d1bb8461f47968
Caturra <wasdjkl233@gmail.com>
2022-11-02 13:16:09 +0800

    update linux/kthread kthreadd

--- linux/proc/kthread/kthreadd.c
+++ linux/proc/kthread/kthreadd.c
@@ -14,20 +14,24 @@ int kthreadd(void *unused)
 	cgroup_init_kthreadd();
 
 	for (;;) {
+		// 当没有创建请求时（kthread_create_list为空），状态为INTERRUPTIBLE
 		set_current_state(TASK_INTERRUPTIBLE);
 		if (list_empty(&kthread_create_list))
 			schedule();
+		// 当需要处理创建请求时，设为RUNNING
 		__set_current_state(TASK_RUNNING);
 
 		spin_lock(&kthread_create_lock);
 		while (!list_empty(&kthread_create_list)) {
 			struct kthread_create_info *create;
 
+			// 拿出一个info
 			create = list_entry(kthread_create_list.next,
 					    struct kthread_create_info, list);
 			list_del_init(&create->list);
 			spin_unlock(&kthread_create_lock);
 
+			// 核心流程
 			create_kthread(create);
 
 			spin_lock(&kthread_create_lock);
@@ -47,6 +51,9 @@ static void create_kthread(struct kthread_create_info *create)
 	current->pref_node_fork = create->node;
 #endif
 	/* We want our own signal handler (we take no signals by default). */
+	// 其实kernel_thread也是走的fork
+	// 创建后的kthread不会直接执行
+	// 在合适的时机kthread被调度时，会执行kthread(create)，create里面有真正需要执行的threadfn
 	pid = kernel_thread(kthread, create, CLONE_FS | CLONE_FILES | SIGCHLD);
 	if (pid < 0) {
 		/* If user was SIGKILLed, I release the structure. */
@@ -57,6 +64,7 @@ static void create_kthread(struct kthread_create_info *create)
 			return;
 		}
 		create->result = ERR_PTR(pid);
+		// 通知完成
 		complete(done);
 	}
 }
