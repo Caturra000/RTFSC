3733bbb08d73399b1f50666b097f88498fbf5b62
Caturra <wasdjkl233@gmail.com>
2022-11-29 16:52:06 +0800

    update linux/sched wake_up_new_task

--- linux/proc/sched/wake_up_new_task.c
+++ linux/proc/sched/wake_up_new_task.c
@@ -1,4 +1,5 @@
 // 文件：/kernel/sched/core.c
+
 /*
  * wake_up_new_task - wake up a newly created task for the first time.
  *
@@ -12,6 +13,7 @@ void wake_up_new_task(struct task_struct *p)
 	struct rq *rq;
 
 	raw_spin_lock_irqsave(&p->pi_lock, rf.flags);
+	// 在前面sched_fork流程完成必要初始化后，从TASK_NEW转换到TASK_RUNNING
 	p->state = TASK_RUNNING;
 #ifdef CONFIG_SMP
 	/*
@@ -23,17 +25,23 @@ void wake_up_new_task(struct task_struct *p)
 	 * as we're not fully set-up yet.
 	 */
 	p->recent_used_cpu = task_cpu(p);
+	// TODO select_task_rq
 	__set_task_cpu(p, select_task_rq(p, task_cpu(p), SD_BALANCE_FORK, 0));
 #endif
 	rq = __task_rq_lock(p, &rf);
 	update_rq_clock(rq);
+	// 求出初始化se的util_avg
 	post_init_entity_util_avg(&p->se);
 
+	// sched入队回调的封装
 	activate_task(rq, p, ENQUEUE_NOCLOCK);
 	p->on_rq = TASK_ON_RQ_QUEUED;
 	trace_sched_wakeup_new(p);
+	// 检查是否满足抢占的一个关键点（其实关键点数目不多）
+	// 满足则打上调度标记
 	check_preempt_curr(rq, p, WF_FORK);
 #ifdef CONFIG_SMP
+	// CFS没有task_woken实现
 	if (p->sched_class->task_woken) {
 		/*
 		 * Nothing relies on rq->lock after this, so its fine to
@@ -47,7 +55,37 @@ void wake_up_new_task(struct task_struct *p)
 	task_rq_unlock(rq, p, &rf);
 }
 
+/*
+ * The caller (fork, wakeup) owns p->pi_lock, ->cpus_allowed is stable.
+ */
+static inline
+int select_task_rq(struct task_struct *p, int cpu, int sd_flags, int wake_flags)
+{
+	lockdep_assert_held(&p->pi_lock);
+
+	if (p->nr_cpus_allowed > 1)
+		cpu = p->sched_class->select_task_rq(p, cpu, sd_flags, wake_flags);
+	else
+		cpu = cpumask_any(&p->cpus_allowed);
+
+	/*
+	 * In order not to call set_task_cpu() on a blocking task we need
+	 * to rely on ttwu() to place the task on a valid ->cpus_allowed
+	 * CPU.
+	 *
+	 * Since this is common to all placement strategies, this lives here.
+	 *
+	 * [ this allows ->select_task() to simply return task_cpu(p) and
+	 *   not worry about this generic constraint ]
+	 */
+	if (unlikely(!is_cpu_allowed(p, cpu)))
+		cpu = select_fallback_rq(task_cpu(p), p);
+
+	return cpu;
+}
+
 // 文件：/kernel/sched/fair.c
+
 /*
  * With new tasks being created, their initial util_avgs are extrapolated
  * based on the cfs_rq's current util_avg:
@@ -74,15 +112,19 @@ void wake_up_new_task(struct task_struct *p)
  * Finally, that extrapolated util_avg is clamped to the cap (util_avg_cap)
  * if util_avg > util_avg_cap.
  */
+// 初始化util_avg流程
 void post_init_entity_util_avg(struct sched_entity *se)
 {
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 	struct sched_avg *sa = &se->avg;
+	// TODO CPU scale计算方式
+	// 这里的cap最终是为了限制util_avg
 	long cpu_scale = arch_scale_cpu_capacity(NULL, cpu_of(rq_of(cfs_rq)));
 	long cap = (long)(cpu_scale - cfs_rq->avg.util_avg) / 2;
 
 	if (cap > 0) {
 		if (cfs_rq->avg.util_avg != 0) {
+			// 初始化sa的util_avg是基于cfs_rq当前util_avg配上自身weight算出来的
 			sa->util_avg  = cfs_rq->avg.util_avg * se->load.weight;
 			sa->util_avg /= (cfs_rq->avg.load_avg + 1);
 
@@ -93,8 +135,10 @@ void post_init_entity_util_avg(struct sched_entity *se)
 		}
 	}
 
+	// 如果se其实是一个task的话
 	if (entity_is_task(se)) {
 		struct task_struct *p = task_of(se);
+		// 由于只跟踪fair sched，这里就略了
 		if (p->sched_class != &fair_sched_class) {
 			/*
 			 * For !fair tasks do:
@@ -112,6 +156,25 @@ void post_init_entity_util_avg(struct sched_entity *se)
 	}
 
 	attach_entity_cfs_rq(se);
+}
+
+static void attach_entity_cfs_rq(struct sched_entity *se)
+{
+	struct cfs_rq *cfs_rq = cfs_rq_of(se);
+
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	/*
+	 * Since the real-depth could have been changed (only FAIR
+	 * class maintain depth value), reset depth properly.
+	 */
+	se->depth = se->parent ? se->parent->depth + 1 : 0;
+#endif
+
+	/* Synchronize entity with its cfs_rq */
+	update_load_avg(cfs_rq, se, sched_feat(ATTACH_AGE_LOAD) ? 0 : SKIP_AGE_LOAD);
+	attach_entity_load_avg(cfs_rq, se, 0);
+	update_tg_load_avg(cfs_rq, false);
+	propagate_entity_cfs_rq(se);
 }
 
 // 文件：/kernel/sched/core.c
