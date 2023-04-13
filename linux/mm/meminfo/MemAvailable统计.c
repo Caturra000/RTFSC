b91219993596e17d421024d8b09145c2cfa68672
Caturra <wasdjkl233@gmail.com>
2023-04-13 17:17:00 +0800

    update linux/meminfo available

--- linux/mm/meminfo/MemAvailable统计.c
+++ linux/mm/meminfo/MemAvailable统计.c
@@ -24,6 +24,10 @@ long si_mem_available(void)
 	 * Estimate the amount of memory available for userspace allocations,
 	 * without causing swapping.
 	 */
+	// global_zone_page_state(NR_FREE_PAGES)对应于MemFree
+	// 预留页比较复杂，可以通过cat /proc/zoneinfo推算
+	//    设每个zone下 zone_reserve = zoneinfo.hi + max_i(protection[i])
+	//    那么totalreserve_pages = \sum zone_reserve
 	// 初步得到的是free - reserved
 	available = global_zone_page_state(NR_FREE_PAGES) - totalreserve_pages;
 
@@ -33,15 +37,18 @@ long si_mem_available(void)
 	 * low watermark worth of cache, needs to stay.
 	 */
 	// 这里pagecache变量是指认为是可以利用的cache
+	// 是LRU中active file + inactive file之和
 	pagecache = pages[LRU_ACTIVE_FILE] + pages[LRU_INACTIVE_FILE];
+	// 除去（所有zone的）低水位（之和）的预留内存，认为都可利用（如果pagecache过小，则只利用一半）
 	pagecache -= min(pagecache / 2, wmark_low);
+	// 继续加上 可利用pagecache 部分
 	available += pagecache;
 
 	/*
 	 * Part of the reclaimable slab consists of items that are in use,
 	 * and cannot be freed. Cap this estimate at the low watermark.
 	 */
-	// 加上slab的reclaimable
+	// 加上slab的部分reclaimable
 	available += global_node_page_state(NR_SLAB_RECLAIMABLE) -
 		     min(global_node_page_state(NR_SLAB_RECLAIMABLE) / 2,
 			 wmark_low);
